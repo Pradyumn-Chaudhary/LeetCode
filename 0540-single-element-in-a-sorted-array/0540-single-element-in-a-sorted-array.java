da7b49fc88aabd93e72b9beafb84f9fcb69bb485
Pradyumn Chaudhary <155794678+Pradyumn-Chaudhary@users.noreply.github.com>
2024-12-23 08:51:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int singleNonDuplicate(int[] arr) {
+        int low = 0,high = arr.length-1;
+        while(low<=high){
+            int mid = (low + high) / 2;
+            if(mid == 0){
+                if(arr[mid]!=arr[mid+1])
+                return arr[mid];
+            }else if(mid == arr.length-1){
+                if(arr[mid]!=arr[arr.length-2])
+                return arr[mid];
+            }else{
+                if(arr[mid] != arr[mid-1] && arr[mid]!=arr[mid+1])
+                return arr[mid];
+                if(arr[mid]==arr[mid-1]){
+                    if(mid%2==0)
+                    high = mid - 1;
+                    else
+                    low = mid +  1;
+                }else{
+                    if(mid%2!=0)
+                    high = mid - 1;
+                    else
+                    low = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
