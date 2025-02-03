2450372019400dc2ba46de6af89c9bc9c600bb95
Pradyumn Chaudhary <155794678+Pradyumn-Chaudhary@users.noreply.github.com>
2025-02-03 07:50:24 +0530

    Time: 1 ms (93.94%), Space: 42.6 MB (82.83%) - LeetHub

--- 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.java
+++ 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -6,6 +6,8 @@ class Solution {
         int md = 1, mi = 1;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] == nums[i - 1]) {
+                md = Math.max(md, de);
+                mi = Math.max(mi, in);
                 de = 1;
                 in = 1;
             } else if (nums[i] > nums[i - 1]) {
@@ -20,6 +22,6 @@ class Solution {
         }
         md = Math.max(md, de);
         mi = Math.max(mi, in);
-        return Math.max(mi,md);
+        return Math.max(mi, md);
     }
 }
