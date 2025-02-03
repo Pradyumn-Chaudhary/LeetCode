4595c2f33424bff6b40f1fa8382326daebfd4d50
Pradyumn Chaudhary <155794678+Pradyumn-Chaudhary@users.noreply.github.com>
2025-02-03 07:58:30 +0530

    Time: 1 ms (93.94%), Space: 42.6 MB (73.06%) - LeetHub

--- 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.java
+++ 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -1,27 +1,20 @@
 class Solution {
     public int longestMonotonicSubarray(int[] nums) {
-        if (nums.length == 0)
-            return 0;
-        int in = 1, de = 1;
-        int md = 1, mi = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
-                md = Math.max(md, de);
-                mi = Math.max(mi, in);
-                de = 1;
-                in = 1;
-            } else if (nums[i] > nums[i - 1]) {
+        if(nums.length == 0)
+        return 0;
+        int de = 1,in = 1,maxLength = 1;
+        for(int i = 1;i < nums.length;i++){
+            if(nums[i]>nums[i-1]){
                 in++;
-                md = Math.max(md, de);
                 de = 1;
-            } else {
+            }else if(nums[i]<nums[i-1]){
                 de++;
-                mi = Math.max(mi, in);
                 in = 1;
+            }else{
+                de = in = 1;
             }
+            maxLength = Math.max(maxLength,Math.max(de,in));
         }
-        md = Math.max(md, de);
-        mi = Math.max(mi, in);
-        return Math.max(mi, md);
+        return maxLength;
     }
 }
