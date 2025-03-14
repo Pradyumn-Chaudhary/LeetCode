d4a17246d1be5355b388074d7a024210747c655e
Pradyumn Chaudhary <155794678+Pradyumn-Chaudhary@users.noreply.github.com>
2025-03-14 21:26:02 +0530

    Time: 0 ms (100%), Space: 43.5 MB (10.41%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,21 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        long nn = n;
+        return calculate(x, nn);
+    }
+
+    public static double calculate(double x, long nn) {
+        if (nn < 0) {
+            x = 1 / x;
+            nn = nn * (-1);
+        }
+        if (nn == 0)
+            return 1;
+        if (nn == 1)
+            return x;
+        if (nn % 2 != 0)
+            return x * calculate(x, nn - 1);
+
+        return calculate(x * x, nn / 2);
     }
 }
