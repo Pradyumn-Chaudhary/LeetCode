63b8e3513f65f8eb00feeff08a1a2a29c0d13d16
Pradyumn Chaudhary <155794678+Pradyumn-Chaudhary@users.noreply.github.com>
2025-03-15 15:31:14 +0530

    Time: 4 ms (82.63%), Space: 69 MB (21.59%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,28 +1,41 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        if(head == null)
-        return true;
-        Stack<ListNode> s = new Stack<>();
-        ListNode curr = head;
-        while(curr != null){
-            s.push(curr);
-            curr = curr.next;
+        if (head == null || head.next == null)
+            return true;
+        ListNode hare = head, turtle = head;
+        while (hare != null && hare.next != null && hare.next.next != null) {
+            hare = hare.next.next;
+            turtle = turtle.next;
         }
-        curr = head;
-        while(curr != null){
-            if(s.pop().val != curr.val)
-            return false;
-            curr = curr.next;
+
+        ListNode prev = null;
+        ListNode curr = turtle.next;
+        while (curr.next != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        curr.next = prev;
+
+        ListNode curr1 = curr;
+        ListNode curr2 = head;
+
+        while (curr1 != null) {
+            if (curr1.val != curr2.val)
+                return false;
+            curr1 = curr1.next;
+            curr2 = curr2.next;
         }
         return true;
     }
